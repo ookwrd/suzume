70f73a65bcc845c607c335568d10b75a4b28f237
luke.mccrohon <luke.mccrohon@localhost>
2011-01-27 19:18:30 +0900

    getNeighbours implemented for OriginalPopulationModel

--- src/PopulationModel.java
+++ src/PopulationModel.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+
 
 public interface PopulationModel {
 
 	public void switchGenerations();
+	
+	public ArrayList<Agent> getNeighbours(Agent agent);
+	public ArrayList<Agent> getNeighbours(Agent agent, int distance);
+	
+	public ArrayList<Agent> getAncestors(Agent agent, int distance);
 	
 }
