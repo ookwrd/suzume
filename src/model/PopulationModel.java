4d9176a5bfd59da6e6d3bb9dcf00106bd525a881
luke.mccrohon <luke.mccrohon@localhost>
2011-01-27 19:46:12 +0900

    getAncestors method added to population model

--- src/model/PopulationModel.java
+++ src/model/PopulationModel.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 
 public interface PopulationModel {
 
-	public void switchGenerations();
+	public void switchGenerations(ArrayList<Agent> agents);
 	
 	public ArrayList<Agent> getNeighbours(Agent agent);
 	public ArrayList<Agent> getNeighbours(Agent agent, int distance);
 	
+	public ArrayList<Agent> getAncestors(Agent agent);
 	public ArrayList<Agent> getAncestors(Agent agent, int distance);
 	
 }
