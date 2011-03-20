58b5906addb44f494c07fb7720d85023dab57324
luke.mccrohon <luke.mccrohon@localhost>
2011-03-20 21:02:30 +0900

    Gradually generalizing APIs to allow greater range of agent types. Trying to incorporate my synonym agent from a previous simulation project.

--- src/model/Agent.java
+++ src/model/Agent.java
@@ -4,24 +4,27 @@ import java.util.ArrayList;
 
 public interface Agent {
 	
-	public void invent();
 
-	//Grammar Learning
+	//Language Learning
 	public void teach(Agent agent);
 	public void learnUtterance(Utterance utterance);
 	public boolean canStillLearn();
 	public Utterance getRandomUtterance();//TODO is this needed externally?
 	
-	//Fitness 
+	//Invention Phase
+	public void invent();
+	
+	//Fitness Calculation
 	public void setFitness(int fitness);
 	public int getFitness();
-	
+	public void communicate(Agent partner); 
 	
 	//Statistics
 	public double geneGrammarMatch();
 	public int numberOfNulls();
 	public int learningIntensity(); //TODO how do i make this more general??
 	
+	//Display
 	public void printAgent();
 	
 	public int getId();
