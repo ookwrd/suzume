163d95e9102a7830ee1812601f5669e55cd15b51
olaf.witkowski <olaf.witkowski@localhost>
2011-03-30 18:28:05 +0900

    minor changes

--- src/Agents/AlteredAgent.java
+++ src/Agents/AlteredAgent.java
@@ -1,19 +1,15 @@
 package Agents;
 
+import java.util.ArrayList;
+
 public class AlteredAgent extends OriginalAgent implements Agent {
+	
+	protected final static int LEFTOVER_RESOURCE_USE = 1;
 	
 	public AlteredAgent(int id) {
 		super(id);
-		// TODO Auto-generated constructor stub
 	}
 
-	public AlteredAgent(OriginalAgent parent1, OriginalAgent parent2, int id) {
-		super(parent1, parent2, id);
-		
-	}
-
-	private final static int LEFTOVER_RESOURCE_USE = 1;
-	
 	public void adjustCosts() {
 		if (learningResource>0) {
 			setFitness(getFitness()+learningResource*LEFTOVER_RESOURCE_USE);
