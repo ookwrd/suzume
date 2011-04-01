c0ead834025bda7581ac105bb3dd41e04608b101
olaf.witkowski <olaf.witkowski@localhost>
2011-04-01 16:17:32 +0900

    Merge

--- src/Agents/AlteredAgent.java
+++ src/Agents/AlteredAgent.java
@@ -4,23 +4,20 @@ import model.RandomGenerator;
 
 public class AlteredAgent extends OriginalAgent implements Agent {
 	
-	public AlteredAgent(OriginalAgent parent1, OriginalAgent parent2, int id, RandomGenerator randomGenerator) {
-		super(parent1, parent2, id, randomGenerator);
-	}
-	
-	public AlteredAgent(int id, RandomGenerator randomGenerator) {
-		super(id, randomGenerator);
-	}
+		protected final static int LEFTOVER_RESOURCE_USE = 1;
+        
+        public AlteredAgent(OriginalAgent parent1, OriginalAgent parent2, int id, RandomGenerator randomGenerator) {
+                super(parent1, parent2, id, randomGenerator);
+        }
+        
+        public AlteredAgent(AgentConfiguration config, int id, RandomGenerator randomGenerator) {
+                super(config, id, randomGenerator);
+        }
 
-	protected final static int LEFTOVER_RESOURCE_USE = 1;
-	
-	
-
-	public void adjustCosts() {
-		if (learningResource>0) {
-			setFitness(getFitness()+learningResource*LEFTOVER_RESOURCE_USE);
-			//learningResource=0;
-		}
-	}
+        public void adjustCosts() {
+                if (learningResource>0) {
+                        setFitness(getFitness()+learningResource*LEFTOVER_RESOURCE_USE);
+                }
+        }
 
 }
