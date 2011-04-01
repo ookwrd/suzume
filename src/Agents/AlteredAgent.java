275650edb55861bb83004d2ddb8bac389619ea54
luke.mccrohon <luke.mccrohon@localhost>
2011-04-01 20:52:40 +0900

    Window resizing.
    Configurable Altered agent.

--- src/Agents/AlteredAgent.java
+++ src/Agents/AlteredAgent.java
@@ -1,29 +1,43 @@
 package Agents;
 
+import java.util.HashMap;
+
 import model.RandomGenerator;
 
 public class AlteredAgent extends OriginalAgent implements Agent {
+	
+	private static HashMap<String, ConfigurationParameter> defaultParameters = new HashMap<String, ConfigurationParameter>(){{
+		put("Leftover Resource Multiplier", new ConfigurationParameter(1.0));
+	}}; 
+	
+	private double resourceMultiplier;
 	
 	public AlteredAgent(){}
 	
 	public void initializeAgent(OriginalAgent parent1, OriginalAgent parent2, int id, RandomGenerator randomGenerator) {
 		super.initializeAgent(parent1, parent2, id, randomGenerator);
+		resourceMultiplier = ((ConfigurationParameter)config.parameters.get("Leftover Resource Multiplier")).getDouble();
 	}
 	
 	public void initializeAgent(AgentConfiguration config, int id, RandomGenerator randomGenerator) {
 		super.initializeAgent(config, id, randomGenerator);
+		resourceMultiplier = ((ConfigurationParameter)config.parameters.get("Leftover Resource Multiplier")).getDouble();
 	}
-
-	protected final static int LEFTOVER_RESOURCE_USE = 1;
 	
 	@Override
 	public String getName(){
 		return "Altered Agent";
 	}
+	
+	@Override
+	public HashMap<String, ConfigurationParameter> getDefaultParameters(){
+		defaultParameters.putAll(super.getDefaultParameters());
+		return defaultParameters;
+	}
 
 	public void adjustCosts() {
 		if (learningResource>0) {
-			setFitness(getFitness()+learningResource*LEFTOVER_RESOURCE_USE);
+			setFitness(getFitness()+(int)(learningResource*resourceMultiplier));
 		}
 	}
 
