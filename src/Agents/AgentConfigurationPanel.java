7b1ecda262c3c7c4faf2bcf56da2922888e34be0
luke.mccrohon <luke.mccrohon@localhost>
2011-04-01 19:36:35 +0900

    Original agent fully configurable.

--- src/Agents/AgentConfigurationPanel.java
+++ src/Agents/AgentConfigurationPanel.java
@@ -1,11 +1,14 @@
 package Agents;
 
+import java.awt.Component;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
 import Agents.AgentConfiguration.AgentType;
@@ -17,8 +20,11 @@ public class AgentConfigurationPanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+		
 	private AgentType type;
+	private HashMap<String, ConfigurationParameter> parameters;
+	private HashMap<String, Component> components;
+
 	protected JPanel innerPanel;
 	
 	public AgentConfigurationPanel(AgentType type){
@@ -35,11 +41,14 @@ public class AgentConfigurationPanel extends JPanel {
 		
 		this.type = type;
 		
-		HashMap<String, ConfigurationParameter> parameters = AgentFactory.constructUninitializedAgent(type).getDefaultParameters();
+		//Get default parameter map for this agent type
+		parameters = AgentFactory.constructUninitializedAgent(type).getDefaultParameters();
+		components = new HashMap<String, Component>();
 		
 		JPanel autoPanel = new JPanel();
 		ConfigurationPanelTools.configurePanel(autoPanel);
 		
+		//For each specified parameter add the appropriate configuration field to the configuration panel.
 		for(Map.Entry<String, ConfigurationParameter> entry : parameters.entrySet()){
 			
 			String key = entry.getKey();
@@ -47,21 +56,27 @@ public class AgentConfigurationPanel extends JPanel {
 			
 			switch (parameter.type) {
 			case Integer:
-				ConfigurationPanelTools.addField(key, parameter.value.toString(), autoPanel);
+				JTextField field = ConfigurationPanelTools.addField(key, parameter.value.toString(), autoPanel);
+				components.put(key, field);
 				break;
 				
 			case Double:
-				ConfigurationPanelTools.addField(key, parameter.value.toString(), autoPanel);
+				JTextField field1 = ConfigurationPanelTools.addField(key, parameter.value.toString(), autoPanel);
+				components.put(key, field1);
 				break;
 				
 			case Boolean:
-				ConfigurationPanelTools.addCheckBox(key, parameter.getBoolean(), autoPanel);
+				JCheckBox box = ConfigurationPanelTools.addCheckBox(key, parameter.getBoolean(), autoPanel);
+				components.put(key, box);
 				break;
 				
 			case String:
-				ConfigurationPanelTools.addField(key, parameter.getString(), autoPanel);
-
+				JTextField field2 = ConfigurationPanelTools.addField(key, parameter.getString(), autoPanel);
+				components.put(key, field2);
+				break;
+				
 			default:
+				System.out.println("Unsupported Configuration Parameter type.");
 				break;
 			}
 			
@@ -74,7 +89,41 @@ public class AgentConfigurationPanel extends JPanel {
 	}
 	
 	public AgentConfiguration getConfiguration(){
-		return new AgentConfiguration(type);
+	
+		HashMap<String, ConfigurationParameter> retParameters = new HashMap<String, ConfigurationParameter>();
+		
+		for(Map.Entry<String, ConfigurationParameter> entry : parameters.entrySet()){
+			
+			String key = entry.getKey();
+			ConfigurationParameter parameter = entry.getValue();
+			
+			Component comp = components.get(key);
+			
+			switch (parameter.type) {
+			
+			case Integer:
+				retParameters.put(key, new ConfigurationParameter(Integer.parseInt(((JTextField)comp).getText())) );
+				break;
+			
+			case Double:
+				retParameters.put(key, new ConfigurationParameter(Double.parseDouble(((JTextField)comp).getText())) );
+				break;
+			
+			case String:
+				retParameters.put(key, new ConfigurationParameter(((JTextField)comp).getText()) );
+				break;				
+				
+			case Boolean:
+				retParameters.put(key, new ConfigurationParameter(((JCheckBox)comp).isSelected()));
+				break;
+				
+			default:
+				break;
+			}
+		
+		}
+		
+		return new AgentConfiguration(type, retParameters);
 	}
 
 }
