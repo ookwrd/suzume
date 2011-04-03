d53cd2139ad8258df79733345110ae4cd4d75a46
luke.mccrohon <luke.mccrohon@localhost>
2011-04-03 21:42:10 +0900

    Print a specified generation of agents.

---
+++ src/model/ModelController.java
@@ -0,0 +1,425 @@
+package model;
+
+import java.awt.print.Printable;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+import Agents.Agent;
+import Agents.AgentFactory;
+import Launcher.Launcher;
+
+public class ModelController implements Runnable {
+        
+        private static final double DEFAULT_DENSITY_GRANULARITY = 0.1;
+
+		private ModelConfiguration config;
+        
+        //statistics
+        private ArrayList<Double>[] totalNumberGenotypes;
+        private ArrayList<Double>[] totalNumberPhenotypes;
+        private ArrayList<Double>[] totalFitnesses;
+        private ArrayList<Double>[] learningIntensities;
+        private ArrayList<Double>[] geneGrammarMatches;
+        private ArrayList<Double>[] numberNulls;
+        
+        private PopulationModel population;
+        
+        private int currentGeneration = 0;
+        private int currentRun = 0;
+        
+        private int printGeneration = 1000;
+        
+        private RandomGenerator randomGenerator;
+
+		private ArrayList<Double> globalGeneGrammarMatches;
+        
+        public ModelController(ModelConfiguration configuration, RandomGenerator randomGenerator){
+                this.config = configuration;
+                this.randomGenerator = randomGenerator;
+                
+                resetModel();
+                
+                totalNumberGenotypes = initializeStatisticsArraylist();
+                totalNumberPhenotypes = initializeStatisticsArraylist();
+                totalFitnesses = initializeStatisticsArraylist();
+                learningIntensities = initializeStatisticsArraylist();
+                geneGrammarMatches = initializeStatisticsArraylist();
+                numberNulls = initializeStatisticsArraylist();    
+                
+                
+        }
+        
+        public void resetModel(){
+
+            population = new OriginalPopulationModel(createIntialAgents(), createIntialAgents());
+        }
+        
+        @SuppressWarnings("unchecked")
+		private ArrayList<Double>[] initializeStatisticsArraylist(){
+        	
+        	ArrayList<Double>[] arrayLists = new ArrayList[config.numberRuns];
+        	for(int i = 0;i < config.numberRuns; i++){
+        		arrayLists[i] = new ArrayList<Double>();
+        	}
+        	
+        	return arrayLists;
+        }
+        
+        /**
+         * Initialize an initial population of agents.
+         * 
+         * @return
+         */
+        private ArrayList<Agent> createIntialAgents(){
+                
+                ArrayList<Agent> agents = new ArrayList<Agent>();
+                for (int i = 1; i <= config.populationSize; i++) {
+                        agents.add(AgentFactory.constructAgent(config.agentConfig, randomGenerator));
+                }
+                
+                return agents;
+        }
+        
+        /**
+         * Main method to run the simulation once constructed. 
+         */
+        public void runSimulation(){
+                
+                for(currentGeneration = 0; currentGeneration < config.generationCount; currentGeneration++){
+                        iterateGeneration();
+                        
+                        //Print progress information
+                        if(currentGeneration % 1000 == 0){
+                        	System.out.println("Run " + currentRun + " Generation " + currentGeneration);
+                        }
+                        
+                        if(currentGeneration == printGeneration){
+                        	System.out.println("Printing Print Generation");
+                        	
+                            for(Agent agent : population.getAncestorGeneration()){  
+                                agent.printAgent();
+                                System.out.println();
+                            }
+                        }
+                }
+                
+                if(++currentRun < config.numberRuns){
+                	resetModel();
+                	runSimulation();
+                }
+        }
+        
+        /**
+         * Runs a single round of the simulation. 
+         */
+        private void iterateGeneration(){
+                
+                training();
+                
+                communication();
+                
+                gatherStatistics();
+                
+                population.switchGenerations(selection());
+                
+        }
+        
+        /**
+         * Training and invention phase of a single round of the simulation.
+         */
+        private void training(){
+                
+                //for each agent
+                for(Agent learner : population.getCurrentGeneration()){
+                        
+                        //get its ancestors (teachers)
+                        ArrayList<Agent> teachers = population.getAncestors(learner, 2);
+                        
+                        for(int i = 0; i < config.criticalPeriod; i++){
+                                
+                                //Get random teacher
+                                Agent teacher = teachers.get(randomGenerator.randomInt(teachers.size()));
+        
+                                teacher.teach(learner);
+                                
+                                if(!learner.canStillLearn()){
+                                        break;
+                                }
+                        }
+        
+                        //Use leftover learning resource to potentially invent new grammar items.
+                        learner.invent();
+                }
+                
+        }
+        
+        /**
+         * Communication Phase  which calculates the fitness of all agents in the population.
+         */
+        private void communication(){
+                
+                for(Agent agent : population.getCurrentGeneration()){
+                
+                        ArrayList<Agent> neighbouringAgents = population.getNeighbors(agent, 1);
+                
+                        //Set the agents fitness to the default base level 
+                        agent.setFitness(config.baseFitness);
+                        
+                        //Communicate with all neighbours
+                        for(Agent neighbour : neighbouringAgents){      
+                                for(int i = 0; i < config.communicationsPerNeighbour; i++){
+                                        agent.communicate(neighbour);
+                                }
+                        }
+                        
+                        agent.adjustCosts();
+                }
+        }
+        
+        /**
+         * Selection and construction of the new generation.
+         * 
+         * @return
+         */
+        private ArrayList<Agent> selection(){
+                
+                ArrayList<Agent> selected = select(config.populationSize*2, population.getCurrentGeneration());
+                
+                ArrayList<Agent> newGenerationAgents = new ArrayList<Agent>();
+                int i = 0;
+                while(newGenerationAgents.size() < config.populationSize){
+                        Agent parent1 = selected.get(i++);
+                        Agent parent2 = selected.get(i++);
+
+                        newGenerationAgents.add(AgentFactory.constructAgent(parent1, parent2, randomGenerator));
+                }
+                
+                return newGenerationAgents;
+        }
+        
+        /**
+         * TODO optimize this class
+         * TODO allow ability to disable the selection of multiples
+         * 
+         * @param toSelect
+         * @param agents
+         * @return
+         */
+        public ArrayList<Agent> select(int toSelect, ArrayList<Agent> agents){
+                
+                ArrayList<Agent> toReturn = new ArrayList<Agent>();
+                
+                //Calculate total fitness of all agents.
+                int totalFitness = 0;
+                for(Agent agent : agents){
+                        totalFitness += agent.getFitness();
+                }
+                
+                //Loop once for each individual
+                for(int i = 0; i < toSelect; i++){
+                
+                        int selectionPoint = randomGenerator.randomInt(totalFitness);
+                        int pointer = 0;
+
+                        for(Agent agent : agents){
+                                //move the pointer along to the next agents borderline
+                                pointer += agent.getFitness();
+                                
+                                //have we gone past the selectionPoint?
+                                if(pointer > selectionPoint){
+                                        toReturn.add(agent);
+                                        break;
+                                }
+                        }
+                }
+                
+                return toReturn;
+        }
+        
+        /**
+         * Gather statistics on the population at this point.
+         */
+        private void gatherStatistics(){
+                
+                ArrayList genotypes = new ArrayList();
+                ArrayList phenotypes = new ArrayList();
+                double antiLearningIntensity = 0;
+                double totalFitness = 0;
+                double genomeGrammarMatch = 0;
+                double numberNull = 0;
+                
+                for(Agent agent : population.getCurrentGeneration()){
+                        
+                        ArrayList<Integer> chromosome = agent.getGenotype();
+                        if (!genotypes.contains(chromosome)){
+                        	genotypes.add(chromosome);
+                        }
+                        
+                        ArrayList<Integer> phenotype = agent.getPhenotype();
+                        if(!phenotypes.contains(phenotype)){
+                        	phenotypes.add(phenotype);
+                        }
+                        
+                        
+                        
+                        
+                        totalFitness += agent.getFitness();
+                        antiLearningIntensity += agent.learningIntensity();
+                        
+                        numberNull += agent.numberOfNulls();
+                        genomeGrammarMatch += agent.geneGrammarMatch();
+                }
+                
+                double learningIntensity = (config.populationSize*2*config.communicationsPerNeighbour - antiLearningIntensity) / config.populationSize / 2 / config.communicationsPerNeighbour;
+             
+                totalFitnesses[currentRun].add(totalFitness/config.populationSize);
+                learningIntensities[currentRun].add(learningIntensity/config.populationSize);
+                geneGrammarMatches[currentRun].add(genomeGrammarMatch/config.populationSize);
+                numberNulls[currentRun].add(numberNull/config.populationSize);
+                totalNumberGenotypes[currentRun].add((double)genotypes.size());
+                totalNumberPhenotypes[currentRun].add((double)phenotypes.size());
+                
+        }
+        
+        
+        @Override
+        public void run(){
+                runSimulation();
+                
+                training();
+                communication();
+                
+                plotStatistics();
+        }
+        
+        private ArrayList<Double>[] trimArrayLists(ArrayList<Double>[] arrays, int start, int finish){
+        	
+        	@SuppressWarnings("unchecked")
+			ArrayList<Double>[] outputArrays = new ArrayList[arrays.length];
+        
+        	for(int i = 0; i < arrays.length && i < finish; i++){
+        		
+        		outputArrays[i] = new ArrayList<Double>();
+        		
+        		for(int j = start; j < arrays[i].size(); j++){
+        			
+        			outputArrays[i].add(arrays[i].get(j));
+        			
+        		}
+        		
+        	}
+        	
+        	return outputArrays;
+        	
+        }
+        
+        
+        /**
+         * Calculate density for an array
+         * @param array
+         */
+        private Hashtable<Double, Integer> calculateDensity(ArrayList<Double>[] array) {
+        	//globalGeneGrammarMatches.addAll(geneGrammarMatches[currentRun]); // total for several runs
+        	
+        	Hashtable<Double, Integer> numOccurrences = 
+        		new Hashtable<Double, Integer>(); // k:value->v:count
+        	double pace = DEFAULT_DENSITY_GRANULARITY;
+        	
+        	for (int i = 0; i < array.length; i++) { // for every run
+	        	for (int j = 0; j < array[0].size(); j++) {
+	        		// cluster value
+	        		double clusterVal = pace*(double) Math.round(array[i].get(j)/pace);
+	        		
+	        		// count
+					if(numOccurrences.containsKey(clusterVal))
+						numOccurrences.put(clusterVal, numOccurrences.get(clusterVal)+1);
+					else
+						numOccurrences.put(clusterVal, 1);
+				}
+        	}
+        	
+        	/*
+        	ArrayList<Double>[] dataSets = new ArrayList[numOccurrences.size()];
+        	for(int j = 0; j<dataSets.length; j++) {
+        		dataSets[0] = new ArrayList<Double>();
+	        	Enumeration<Double> e = numOccurrences.keys();
+	        	int i = 0;
+	        	while(e.hasMoreElements()) {
+	        		Double val = e.nextElement();
+	        		//dataSets[0].add(i, (double) val);
+	        		dataSets[j].add(i, (double) numOccurrences.get(val));
+	        		i++;
+	        	}
+	        	
+        	}*/
+        	return numOccurrences;
+        }
+        
+        /**
+         * 
+         */
+        private void plotStatistics() {
+        	ModelStatistics densityWindow = new ModelStatistics("[Seed: " + randomGenerator.getSeed() + "   " + config + "]");
+        	String printName = config.printName().replaceAll("  "," ").replaceAll("  "," ").replaceAll(":", "").replaceAll(" ", "-");
+        	
+        	densityWindow.plot(geneGrammarMatches, "Gene Grammar Matches", "Occurrences", "Gene Grammar Matches", printName);
+        	densityWindow.plot(calculateDensity(geneGrammarMatches), "Density (Gene Grammar Matches)", "Occurrences", "Gene Grammar Matches", printName);
+        	densityWindow.plot(calculateDensity(trimArrayLists(geneGrammarMatches,200,geneGrammarMatches[0].size())), "200 onwards...Density (Gene Grammar Matches)", "Occurrences", "Gene Grammar Matches", printName);
+        	
+        	densityWindow.plot(learningIntensities, "Learning Intensity", "Occurrences", "Learning Intensity", printName);
+        	densityWindow.plot(calculateDensity(learningIntensities), "Density (Learning Intensity)", "Occurrences", "Learning Intensity", printName);
+        	densityWindow.plot(numberNulls, "Number of Nulls", "Occurrences", "Number of Nulls", printName);
+        	densityWindow.plot(totalFitnesses, "Fitnesses", "Occurrences", "Fitnesses", printName);
+        	densityWindow.plot(totalNumberGenotypes, "Number of Genotypes", "Occurrences", "Number of Genotypes", printName);
+        	densityWindow.plot(totalNumberPhenotypes, "Number of Phenotypes", "Occurrences", "Number of Phenotypes", printName);
+        	
+        	densityWindow.display();
+        }
+        
+        public static void main2(String[] args){
+                
+                //Test selection
+                ModelController selector = new ModelController(new ModelConfiguration(), new RandomGenerator());
+
+                System.out.println("Using seed:" + selector.randomGenerator.getSeed());
+                
+                //TODO print other simulation parameters
+                System.out.println();
+                
+                selector.runSimulation();
+                
+                //Get to a point where the current generation has calculated fitness. TODO swap out with previous generation.
+                selector.training();
+                selector.communication();
+                
+                for(Agent agent : selector.population.getCurrentGeneration()){  
+                        agent.printAgent();
+                        System.out.println();
+                }
+                
+                System.out.println();
+                System.out.println("Fitnesses\tlearningResc\tGeneGrammarMatch\tNulls");
+                for(int i = 0; i < selector.learningIntensities[0].size(); i++){
+                        System.out.println(selector.totalFitnesses[0].get(i) + "\t" + selector.learningIntensities[0].get(i) + "\t" + selector.geneGrammarMatches[0].get(i) + "\t" + selector.numberNulls[0].get(i));
+                }
+                
+                //Plot
+                selector.plotStatistics();
+                
+                for(Agent agent : selector.population.getCurrentGeneration()){
+                        System.out.println(agent.getGenotype());
+                }
+                
+                Object double1 = new Double(12342.09);
+                
+                System.out.println(double1);
+        }
+        
+        public static void main(String[] args) {
+			new Launcher();
+		}
+
+}
---
+++ src/model/ModelStatistics.java
@@ -0,0 +1,288 @@
+//TODO where did this come from.
+
+package model;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.CategoryItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+@SuppressWarnings("serial")
+public class ModelStatistics extends JFrame {
+
+	public int printSize = 200;
+	public int viewSize = 100;
+	public int[] imageRatio = { 5, 3 };
+
+	private final static boolean SAVE_WHILE_PROCESSING = false;
+	private static final int LINE_CHART = 1;
+	private static final int BAR_CHART = 2;
+
+	private String experimentId = "default";
+	private String yLabel = "";
+	private String xLabel = "";
+	private String title = "A chart";
+
+	JPanel innerPane = new JPanel();
+	JScrollPane scroller = new JScrollPane(innerPane);
+
+	public ModelStatistics(String title) {
+		innerPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		scroller.setVerticalScrollBar(new JScrollBar());
+
+		this.add(scroller, BorderLayout.CENTER);
+		this.setTitle(title);
+		this.setSize(new Dimension(1000, 500));
+	}
+
+	public void display() {
+		setVisible(true);
+	}
+
+	public void plot(ArrayList<Double>[] dataSets, String title, String yLabel,
+			String xLabel, String experimentId) {
+		this.experimentId = experimentId;
+		this.title = title;
+		this.yLabel = yLabel;
+		this.xLabel = xLabel;
+		XYSeries[] newSeries = new XYSeries[dataSets.length];
+
+		for (int j = 0; j < dataSets.length; j++) {
+
+			newSeries[j] = new XYSeries(yLabel + j);
+
+			ArrayList<Double> data = dataSets[j];
+
+			for (int i = 0; i < data.size(); i++) {
+				newSeries[j].add(new Double(
+
+				i // x
+						), new Double(
+
+						data.get(i) // y
+						));
+			}
+
+		}
+
+		innerPane.add(new JLabel(new ImageIcon(createImage(newSeries))));
+		innerPane.validate();
+
+		saveChart(dataSets[0]);// TODO
+	}
+
+	public void plot(Hashtable<Double, Integer> table, String title,
+			String yLabel, String xLabel, String experimentId) {
+		this.experimentId = experimentId;
+		this.title = title;
+		this.yLabel = yLabel;
+		this.xLabel = xLabel;
+		XYSeries[] newSeries = new XYSeries[1];
+		newSeries[0] = new XYSeries(yLabel);
+
+		Enumeration<Double> e = table.keys();
+		while (e.hasMoreElements()) {
+			Double key = (Double) e.nextElement();
+			newSeries[0].add(new Double(key // x
+					), new Double(table.get(key) // y
+					));
+		}
+
+		innerPane.add(new JLabel(new ImageIcon(createImage(newSeries))));
+		innerPane.validate();
+
+		// saveChart(table);
+	}
+
+	private JFreeChart createChart(XYSeries[] series, int type) {
+
+		XYSeriesCollection dataset = new XYSeriesCollection();
+
+		for (int i = 0; i < series.length; i++) {
+			dataset.addSeries(series[i]);
+		}
+
+		JFreeChart chart;
+		switch (type) {
+		case LINE_CHART:
+			chart = ChartFactory.createXYLineChart(title, // Title
+					xLabel, // X-Axis label
+					yLabel, // Y-Axis label
+					dataset, // Dataset
+					PlotOrientation.VERTICAL, // Plot orientation
+					false, // Show legend
+					false, // Tooltips
+					false); // URL;
+			break;
+
+		case BAR_CHART:
+			chart = ChartFactory.createBarChart(title, // Title
+					xLabel, // X-Axis label
+					yLabel, // Y-Axis label
+					(CategoryDataset) dataset, // Dataset
+					PlotOrientation.VERTICAL, // Plot orientation
+					false, // Show legend
+					false, // Tooltips
+					false); // URL
+			break;
+
+		default:
+			chart = ChartFactory.createXYLineChart(title, // Title
+					xLabel, // X-Axis label
+					yLabel, // Y-Axis label
+					dataset, // Dataset
+					PlotOrientation.VERTICAL, // Plot orientation
+					false, // Show legend
+					false, // Tooltips
+					false); // URL;
+		}
+
+		return chart;
+	}
+
+	private BufferedImage createImage(XYSeries[] series) {
+		return createImage(series, yLabel, SAVE_WHILE_PROCESSING);
+	}
+
+	/**
+	 * 
+	 * @param series
+	 * @param title
+	 * @param printToFile
+	 *            : if true, the corresponding file is created
+	 * @return
+	 */
+	private BufferedImage createImage(XYSeries[] series, String title,
+			boolean printToFile) {
+		JFreeChart chart = createChart(series, LINE_CHART);
+		BufferedImage image = chart.createBufferedImage(imageRatio[0]
+				* viewSize, imageRatio[1] * viewSize);
+
+		JLabel lblChart = new JLabel();
+		lblChart.setIcon(new ImageIcon(image));
+
+		// Creating the corresponding file
+		if (printToFile) {
+			BufferedImage imagePrint = chart.createBufferedImage(imageRatio[0]
+					* printSize, imageRatio[1] * printSize);
+			createFile(chart, imagePrint);
+		}
+
+		return image;
+	}
+
+	/**
+	 * Create a jpg file for the image of the chart
+	 * 
+	 * @param chart
+	 * @param image
+	 */
+	private void createFile(JFreeChart chart, BufferedImage image) {
+
+		cd("/");
+		mkdir("/suzume-charts");
+		try {
+
+			ChartUtilities.saveChartAsJPEG(
+					new File("/suzume-charts/" + yLabel.replaceAll(" ", "_")
+							+ "-" + experimentId + ".jpg"), chart,
+					imageRatio[0] * printSize, imageRatio[1] * printSize);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create a directory
+	 * 
+	 * @param name
+	 */
+	private void cd(String name) {
+		System.setProperty("user.dir", name);
+		System.out.println(System.getProperty("user.dir"));
+	}
+
+	/**
+	 * This only prints charts to file without showing a window
+	 */
+	private void saveChart(ArrayList<Double> data) {
+
+		XYSeries[] series = new XYSeries[1];
+		series[0] = new XYSeries(yLabel);
+
+		for (int i = 0; i < data.size(); i++) {
+			series[0].add(new Double(
+
+			i // x
+					), new Double(
+
+					data.get(i) // y
+					));
+		}
+
+		JFreeChart chart = createChart(series, LINE_CHART);
+		BufferedImage imagePrint = chart.createBufferedImage(imageRatio[0]
+				* printSize, imageRatio[1] * printSize);
+		createFile(chart, imagePrint);
+
+	}
+
+	/**
+	 * Create a directory
+	 * 
+	 * @param dir
+	 */
+	private void mkdir(String dir) {
+
+		try {
+			boolean success = (new File(dir)).mkdir();
+			if (success) {
+				System.out.println("Folder " + dir + " created");
+			}
+
+		} catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+		}
+
+	}
+
+	private void displayChart(BufferedImage image) {
+		try {
+			System.out.println("Enter image name\n");
+			BufferedReader bf = new BufferedReader(new InputStreamReader(
+					System.in));
+			String imageName = bf.readLine();
+			File input = new File(imageName);
+			image = ImageIO.read(input);
+		} catch (IOException ie) {
+			System.out.println("Error:" + ie.getMessage());
+		}
+	}
+
+}
