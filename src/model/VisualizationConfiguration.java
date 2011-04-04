7aef4a94b4978c76d75d7661b93bae40c58c8ccc
luke.mccrohon <luke.mccrohon@localhost>
2011-04-05 03:44:50 +0900

    Enable Continuous Variation configuration parameter added.

--- src/model/VisualizationConfiguration.java
+++ src/model/VisualizationConfiguration.java
@@ -9,6 +9,7 @@ public class VisualizationConfiguration {
 	
 	public static final boolean DEFAULT_PRINT_GENERATIONS = true;
 	public static final int DEFAULT_PRINT_GENERATIONS_EACH_X = 1000;
+	public static final boolean DEFAULT_ENABLE_CONTINUOUS_VISUALIZATION = false;
 	
 	public boolean printSliceGeneration;
 	public int sliceGeneration;
@@ -16,18 +17,22 @@ public class VisualizationConfiguration {
 	public boolean printGenerations;
 	public int printGenerationsEachX;
 	
+	public boolean enableContinuousVisualization;
+	
 	public VisualizationConfiguration(){
 		this.printSliceGeneration = DEFAULT_PRINT_SLICE_GENERATION;
 		this.sliceGeneration = DEFAULT_SLICE_GENERATION;
 		this.printGenerations = DEFAULT_PRINT_GENERATIONS;
 		this.printGenerationsEachX = DEFAULT_PRINT_GENERATIONS_EACH_X;
+		this.enableContinuousVisualization = DEFAULT_ENABLE_CONTINUOUS_VISUALIZATION;
 	}
 	
-	public VisualizationConfiguration(boolean printSliceGeneration, int sliceGeneration, boolean printGenerations, int printGenerationsEachX){
+	public VisualizationConfiguration(boolean printSliceGeneration, int sliceGeneration, boolean printGenerations, int printGenerationsEachX, boolean enableContinuousVisualization){
 		this.printSliceGeneration = printSliceGeneration;
 		this.sliceGeneration = sliceGeneration;
 		this.printGenerations = printGenerations;
 		this.printGenerationsEachX = printGenerationsEachX;
+		this.enableContinuousVisualization = enableContinuousVisualization;
 	}
 	
 	public VisualizationConfiguration(StringTokenizer tokenizer){
