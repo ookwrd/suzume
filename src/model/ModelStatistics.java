fd64ec47941967fe5368151c1be4b26c4f3c0089
luke.mccrohon <luke.mccrohon@localhost>
2011-04-14 03:32:46 +0900

    Remaining chart related functionality moved to ChartPanel

--- src/model/ModelStatistics.java
+++ src/model/ModelStatistics.java
@@ -6,15 +6,10 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
@@ -23,15 +18,7 @@ import tools.Pair;
 import model.ChartPanel.ChartType;
 
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.JFreeChart;
 import org.jfree.chart.StandardChartTheme;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYBarRenderer;
-import org.jfree.chart.renderer.xy.XYDotRenderer;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 
 @SuppressWarnings("serial")
 public class ModelStatistics extends JPanel {
@@ -85,84 +72,12 @@ public class ModelStatistics extends JPanel {
 		
 		ChartType type = ChartType.SCATTER_PLOT;//TODO
 		
-		//Testing
 		ChartPanel chartPanel = new ChartPanel(table, type, title, yLabel, xLabel, experiment);
 		add(chartPanel);
 		chartPanels.add(chartPanel);
-		
-		
-		XYSeries[] newSeries = new XYSeries[table.length];
-		for(int i = 0; i < newSeries.length; i++){
-			newSeries[i] = new XYSeries(yLabel);
-		}
 
-		for(int i = 0; i < newSeries.length; i++){
-			for(int j = 0; j < table[i].size(); j++){
-				Pair<Double, Double> value = table[i].get(j);
-				newSeries[i].add(value.first, value.second);
-			}
-		}
-
-		JFreeChart chart = createChart(newSeries, type, title, xLabel, yLabel);
-		
 		validate();
 		frame.validate();
-	}
-	
-	private JFreeChart createChart(XYSeries[] series, ChartType type, String title, String xLabel, String yLabel) {
-
-		XYSeriesCollection dataset = new XYSeriesCollection();
-
-		for (int i = 0; i < series.length; i++) {
-			dataset.addSeries(series[i]);
-		}
-
-		JFreeChart chart;
-		switch (type) {
-
-		case HISTOGRAM:
-			chart = ChartFactory.createHistogram(title, // Title
-					xLabel, // X-Axis label
-					yLabel, // Y-Axis label
-					dataset, // Dataset
-					PlotOrientation.VERTICAL, // Plot orientation
-					false, // Show legend
-					false, // Tooltips
-					false); // URL
-			XYPlot catPlot = chart.getXYPlot();
-			((XYBarRenderer)catPlot.getRenderer()).setShadowVisible(false);
-			break;
-			
-		case SCATTER_PLOT:
-			
-			chart = ChartFactory.createScatterPlot(title, 
-					xLabel, 
-					yLabel, 
-					dataset, 
-					PlotOrientation.VERTICAL, 
-					false, 
-					false, 
-					false);
-			XYPlot plot = (XYPlot)chart.getPlot();
-			XYDotRenderer renderer = new XYDotRenderer();
-			renderer.setDotWidth(2);
-			renderer.setDotHeight(2);
-			plot.setRenderer(renderer);
-			break;
-
-		case LINE_CHART:
-		default:
-			chart = ChartFactory.createXYLineChart(title, // Title
-					xLabel, // X-Axis label
-					yLabel, // Y-Axis label
-					dataset, // Dataset
-					PlotOrientation.VERTICAL, // Plot orientation
-					false, // Show legend
-					false, // Tooltips
-					false); // URL;
-		}
-
-		return chart;
 	}
 	
 	/**
