2d31747664437fc7b4193a77c016c399de1aa204
luke.mccrohon <luke.mccrohon@localhost>
2011-04-15 03:24:03 +0900

    histograms fixed.

---
+++ src/model/ChartPanel.java
@@ -0,0 +1,260 @@
+package model;
+
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.ClusteredXYBarRenderer;
+import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.chart.renderer.xy.XYDotRenderer;
+import org.jfree.data.statistics.HistogramDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import tools.Pair;
+import tools.Statistics;
+
+@SuppressWarnings("serial")
+public class ChartPanel extends JPanel {
+
+	private static final Dimension SAVE_DIMENSION = new Dimension(1000, 600);
+	private static final Dimension THUMBNAIL_DIMENSION = new Dimension(500,300);
+	
+	public static enum ChartType {LINE_CHART, HISTOGRAM, SCATTER_PLOT, AREA_CHART};
+	
+	private JFreeChart chart;
+	private String filename;
+	
+	public ChartPanel(ArrayList<Pair<Double, Double>> data, ChartType type, String title,
+			String yLabel, String xLabel, String printName){
+		this(wrapArrayList(data), type, title, yLabel, xLabel, printName);			
+	}
+	
+	public ChartPanel(ArrayList<Pair<Double, Double>>[] data, ChartType type, String title,
+			String yLabel, String xLabel, String printName){
+		super();
+		
+		this.filename = title.replaceAll(" ", "") + "-" + printName + ".jpg";
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		this.chart = createChart(data, type, title, xLabel, yLabel);
+		add(createImageJLabel(chart));
+		
+	}
+	
+	public void addAdditionalChart(ArrayList<Pair<Double, Double>>[] data, ChartType type, String title,
+			String yLabel, String xLabel){
+		JFreeChart chart = createChart(data, type, title, xLabel, yLabel);
+		add(createImageJLabel(chart));
+	}
+	
+	private JLabel createImageJLabel(JFreeChart chart) {
+		
+		BufferedImage image = chart.createBufferedImage(
+				THUMBNAIL_DIMENSION.width, 
+				THUMBNAIL_DIMENSION.height
+				);
+
+		JLabel chartLabel = new JLabel();
+		chartLabel.setIcon(new ImageIcon(image));
+
+		return chartLabel;
+	}
+	
+	private static <E> ArrayList<E>[] wrapArrayList(ArrayList<E> input){
+		@SuppressWarnings("unchecked")
+		ArrayList<E>[] wrapper = new ArrayList[1];
+		wrapper[0] = input;
+		return wrapper;
+	}
+	
+	public JFreeChart getChart() {
+		return chart;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	private JFreeChart createChart(ArrayList<Pair<Double, Double>>[] series, ChartType type, String title, String xLabel, String yLabel){
+		
+		JFreeChart chart;
+		switch (type) {
+
+		case HISTOGRAM:
+			chart = ChartFactory.createHistogram(title, 
+					xLabel,
+					yLabel, 
+					createHistogramDataset(series), 
+					PlotOrientation.VERTICAL,
+					false, 
+					false, 
+					false
+					);
+			XYPlot catPlot = chart.getXYPlot();
+            ((XYBarRenderer)catPlot.getRenderer()).setShadowVisible(false);
+			break;
+		
+		case AREA_CHART:
+			chart = ChartFactory.createXYAreaChart(title, // Title
+					xLabel, // X-Axis label
+				//	false,
+					yLabel, // Y-Axis label
+					createXyDataset(series), // Dataset
+					PlotOrientation.VERTICAL, // Plot orientation
+					false, // Show legend
+					false, // Tooltips
+					false// URL
+					); 
+			break;
+			
+		case SCATTER_PLOT:
+			
+			chart = ChartFactory.createScatterPlot(title, 
+					xLabel, 
+					yLabel, 
+					createXyDataset(series), 
+					PlotOrientation.VERTICAL, 
+					false, 
+					false, 
+					false
+					);
+			XYPlot plot = (XYPlot)chart.getPlot();
+			XYDotRenderer renderer = new XYDotRenderer();
+			renderer.setDotWidth(2);
+			renderer.setDotHeight(2);
+			plot.setRenderer(renderer);
+			break;
+
+		case LINE_CHART:
+		default:
+			chart = ChartFactory.createXYLineChart(title,
+					xLabel,
+					yLabel,
+					createXyDataset(series),
+					PlotOrientation.VERTICAL, 
+					false, // Show legend
+					false, // Tooltips
+					false); // URL;
+		}
+
+		return chart;
+
+	}
+	
+	private XYSeriesCollection createXyDataset(ArrayList<Pair<Double, Double>>[] data){
+		
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		
+		int key = 0;
+		for(ArrayList<Pair<Double, Double>> series : data){
+			dataset.addSeries(createXySeries(series, key++));
+		}
+		
+		return dataset;
+	}
+	
+	private XYSeries createXySeries(ArrayList<Pair<Double, Double>> data, Integer key){
+		
+		XYSeries series = new XYSeries(key);
+		for(Pair<Double, Double> value : data){
+			series.add(value.first, value.second);
+		}
+		
+		return series;
+	}
+	
+	private HistogramDataset createHistogramDataset(ArrayList<Pair<Double, Double>>[] series){
+		
+	
+		HistogramDataset dataSet = new HistogramDataset();
+		
+		dataSet.addSeries(new Double(1), stripValues(series), 1000);
+		
+		return dataSet;
+	}
+	
+	//TODO factor this into Statistics class
+	private double[] stripValues(ArrayList<Pair<Double, Double>>[] series){
+		
+		ArrayList<Double> dataset = new ArrayList<Double>();
+		
+		for(ArrayList<Pair<Double, Double>> list : series){
+			for(Pair<Double, Double> pair : list){
+				dataset.add(pair.second);
+			}
+		}
+		
+		//Convert to doubles
+		double[] retVal = new double[dataset.size()];
+		for(int i =0; i < dataset.size(); i++){
+			retVal[i] = dataset.get(i);
+		}
+		
+		return retVal;
+	}
+	
+	public void saveThumbNailSizeChart(){
+		saveChartToFile(THUMBNAIL_DIMENSION);
+	}
+	
+	public void saveFullSizeChart(){
+		saveChartToFile(SAVE_DIMENSION);
+	}
+
+	public void saveChartToFile(Dimension printSize) {
+
+		cd("/");
+		mkdir("/suzume-charts");//TODO extract to configuration file
+		try {
+
+			ChartUtilities.saveChartAsJPEG(
+					new File("/suzume-charts/" + filename), 
+					chart,
+					printSize.width, 
+					printSize.height
+					);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Change to a directory
+	 * 
+	 * @param name
+	 */
+	private void cd(String name) {
+		System.setProperty("user.dir", name);
+	}
+	
+	/**
+	 * Create a directory
+	 * 
+	 * @param dir
+	 */
+	private void mkdir(String dir) {
+
+		try {
+			boolean success = (new File(dir)).mkdir();
+			if (success) {
+				System.out.println("Folder " + dir + " created");
+			}
+		} catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+	
+}
---
+++ src/tools/Statistics.java
@@ -0,0 +1,182 @@
+package tools;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import tools.Pair;
+
+public class Statistics {
+	
+
+	public static final int NUMBER_DENSITY_BUCKETS = 1000;
+
+	/**
+	 * Convenience  method for printing an arraylist of pairs as a vertical list.
+	 * 
+	 * @param <E>
+	 * @param <J>
+	 * @param series
+	 */
+	public static <E extends Comparable<E>, J> void printPairList(ArrayList<Pair<E, J>> series){
+		
+		System.out.println("Printing Pair list:");		
+		for(Pair<E, J> pair : series){
+			System.out.println(pair);
+		}
+		
+	}
+
+	/**
+	 * Find the set of keys that correspond to local minima in the input data 
+	 * 
+	 * @param series
+	 * @return
+	 */
+	public static ArrayList<Double> findLocalMinima(ArrayList<Pair<Double, Integer>> series){
+		
+		ArrayList<Double> retVal = new ArrayList<Double>();
+		Collections.sort(series);
+		
+		for(int i = 1; i < series.size()-1; i++){	
+			Pair<Double, Integer> pair =series.get(i);
+			if(pair.second < series.get(i-1).second && 
+					pair.second < series.get(i+1).second){
+				retVal.add(pair.first);
+			}	
+		}
+		
+		return retVal;
+	}
+
+	/**
+	 * Calculates moving average of data set for all points extending to a distance of windowRadius around the centre point.
+	 * 
+	 * @param series
+	 * @param windowRadius
+	 * @return
+	 */
+	public static ArrayList<Pair<Double, Double>> movingAverageSmoothing(ArrayList<Pair<Double, Integer>> series, int windowRadius){
+		
+		ArrayList<Pair<Double, Double>> retVal = new ArrayList<Pair<Double,Double>>();
+		Collections.sort(series);
+		
+		double count = 0;
+	
+		for(int i = windowRadius; i < series.size() - windowRadius; i++){
+				
+			for(int j = 0; j < windowRadius; j++){
+				count += series.get(i-j).second;
+				count += series.get(i+j).second;
+			}
+				
+			count += series.get(i).second;
+			
+			retVal.add(new Pair<Double, Double>(series.get(i).first, (count/(windowRadius*2+1))));
+		}
+		
+		return retVal;
+		
+	}
+
+	/**
+	 * Turn the discrete data points in inputArray into a set of value countPairs.
+	 * 
+	 * @param inputArray
+	 */
+	public static ArrayList<Pair<Double, Double>>[] calculateDensity(ArrayList<Pair<Double, Double>> inputArray) {
+	
+		// find range
+		Double min = Double.MAX_VALUE;
+		Double max = Double.MIN_VALUE;
+			for (int j = 0; j < inputArray.size(); j++) {
+			Double value = inputArray.get(j).second;
+			if (value > max) {
+				max = value;
+			}
+			if(value < min){
+				min = value;
+			}
+		}
+			
+		double range = max - min;
+		double step = range/NUMBER_DENSITY_BUCKETS;
+		ArrayList<Integer> numOccurrences = new ArrayList<Integer>();
+	
+		for(int i = 0; i < NUMBER_DENSITY_BUCKETS+1; i++){//TODO why the plus 1?
+			numOccurrences.add(0);
+		}
+		
+		for(Pair<Double, Double> value : inputArray){
+			int index = (int)((value.second-min)/step);
+			numOccurrences.set(index, numOccurrences.get(index)+1);
+		}
+		
+		ArrayList<Pair<Double, Double>>[] retVal = new ArrayList[1];
+		retVal[0] = new ArrayList<Pair<Double,Double>>();
+		
+		for(int i = 0; i < numOccurrences.size(); i++){
+			double reconstruct = min + i * step;
+			retVal[0].add(new Pair<Double, Double>(reconstruct, numOccurrences.get(i).doubleValue()));
+		}
+		
+		return retVal;
+	}
+
+	
+	/**
+	 * Internally statistics are handled as time ordered arraylists, this method allows the user to 
+	 * take a subsection of an array to focus on a section of interest.
+	 * 
+	 * @param arrays
+	 * @param start
+	 * @param finish
+	 * @return
+	 */
+	public static <E> ArrayList<E> trimArrayList(ArrayList<E> array, int start, int finish){
+		ArrayList<E>[] wrapper = new ArrayList[1];
+		wrapper[0] = array;
+		return trimArrayLists(wrapper, start, finish)[0];
+	}
+		
+	/**
+	 * Internally statistics are handled as time ordered arraylists, this method allows the user to 
+	 * take a subsection of these arrays to focus on a section of interest.
+	 * 
+	 * @param arrays
+	 * @param start
+	 * @param finish
+	 * @return
+	 */
+	public static <E> ArrayList<E>[] trimArrayLists(ArrayList<E>[] arrays, int start, int finish){
+	
+		@SuppressWarnings("unchecked")
+		ArrayList<E>[] outputArrays = new ArrayList[arrays.length];
+	
+		for(int i = 0; i < arrays.length && i < finish; i++){
+			outputArrays[i] = new ArrayList<E>();
+	
+			for(int j = start; j < arrays[i].size(); j++){
+				outputArrays[i].add(arrays[i].get(j));
+			}
+		}
+	
+		return outputArrays;
+	}
+
+	/**
+	 * Helper method for aggregating together an array of arraylists into a single arraylist
+	 * 
+	 * @param <E>
+	 * @param arrayLists
+	 * @return
+	 */
+	public static <E> ArrayList<E> aggregateArrayLists(ArrayList<E>[] arrayLists){
+		
+		ArrayList<E> retVal = new ArrayList<E>();
+		
+		for(ArrayList<E> arrayList : arrayLists){
+			retVal.addAll(arrayList);
+		}
+		
+		return retVal;
+	}
+}
