44f6063c3c9300ab9e53c00e741c0225877d692d
olaf.witkowski <olaf.witkowski@localhost>
2011-04-14 17:49:23 +0900

    merging

--- src/tools/Clustering.java
+++ src/tools/Clustering.java
@@ -5,6 +5,10 @@ import java.util.Hashtable;
 
 public interface Clustering {
 	
+	public static final int MAX_NUM_CLUSTERS = 10;
+
+	public static final int MIN_NUM_CLUSTERS = 4;
+
 	public Hashtable<Double, Integer> cluster(ArrayList<Double>[] array);
 
 }
