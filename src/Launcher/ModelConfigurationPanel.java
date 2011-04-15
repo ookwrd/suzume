7a396d742434b4ad303bf453e08298149dfff5a9
luke.mccrohon <luke.mccrohon@localhost>
2011-04-15 18:28:33 +0900

    Choice between multiple selection models.

---
+++ src/Launcher/ModelConfigurationPanel.java
@@ -0,0 +1,72 @@
+package Launcher;
+
+import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+import model.ModelConfiguration;
+import model.ModelConfiguration.PopulationModelType;
+import model.SelectionModel.SelectionModels;
+
+@SuppressWarnings("serial")
+public class ModelConfigurationPanel extends JPanel {
+
+	private AgentTypeConfigurationPanel agentConfigurationPanel;
+	
+	private JPanel innerPanel;
+	
+	private JComboBox populationModelTypeBox;
+	private JComboBox selectionModelTypeBox;
+	
+	private JTextField generationCountField;
+	private JTextField populationSizeField;
+	
+	private JTextField baseFitnessField;
+	private JTextField communicationPerNeighbourField;
+	
+	private JTextField criticalPeriodField;
+
+	private JTextField numberRunsField;
+	
+	public ModelConfigurationPanel(){
+		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(new TitledBorder("Model Configuration:"));
+		
+		agentConfigurationPanel = new AgentTypeConfigurationPanel();
+		add(agentConfigurationPanel);
+		
+		innerPanel = new JPanel();
+		add(innerPanel);
+		
+		ConfigurationPanelTools.configurePanel(innerPanel);
+		
+		populationModelTypeBox = ConfigurationPanelTools.addComboBox("Population Model:", PopulationModelType.values(), innerPanel);
+		selectionModelTypeBox= ConfigurationPanelTools.addComboBox("Selection Model:", SelectionModels.values(), innerPanel);
+		generationCountField = ConfigurationPanelTools.addField("Number of Generations:", ""+ModelConfiguration.DEFAULT_GENERATION_COUNT, innerPanel);
+		populationSizeField = ConfigurationPanelTools.addField("Population Size:", ""+ModelConfiguration.DEFAULT_POPULATION_SIZE, innerPanel); 
+		baseFitnessField= ConfigurationPanelTools.addField("Base fitness value:", ""+ModelConfiguration.DEFAULT_BASE_FITNESS, innerPanel);
+		communicationPerNeighbourField = ConfigurationPanelTools.addField("CommunicationsPerNeighbour:", ""+ModelConfiguration.DEFAULT_COMMUNICATIONS_PER_NEIGHBOUR, innerPanel);
+		criticalPeriodField = ConfigurationPanelTools.addField("Critical Period:", ""+ModelConfiguration.DEFAULT_CRITICAL_PERIOD, innerPanel);
+		numberRunsField = ConfigurationPanelTools.addField("Rumber of Runs", ""+ModelConfiguration.DEFAULT_NUMBER_RUNS,innerPanel);
+		
+		ConfigurationPanelTools.makeGrid(innerPanel);
+	}
+	
+	public ModelConfiguration getConfiguration(){
+		
+		return new ModelConfiguration(
+				agentConfigurationPanel.getConfiguration(),
+				(PopulationModelType)populationModelTypeBox.getSelectedItem(),
+				(SelectionModels)selectionModelTypeBox.getSelectedItem(),
+				Integer.parseInt(generationCountField.getText().trim()),
+				Integer.parseInt(populationSizeField.getText().trim()),
+				Integer.parseInt(baseFitnessField.getText().trim()),
+				Integer.parseInt(communicationPerNeighbourField.getText().trim()),
+				Integer.parseInt(criticalPeriodField.getText().trim()),
+				Integer.parseInt(numberRunsField.getText().trim()));
+	}
+	
+}
---
+++ src/model/ModelStatistics.java
@@ -0,0 +1,158 @@
+
+package model;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.StandardChartTheme;
+
+import tools.Pair;
+import tools.Statistics;
+
+import model.ChartPanel.ChartType;
+
+
+@SuppressWarnings("serial")
+public class ModelStatistics extends JPanel {
+
+	public enum PlotType {TIMESERIES, DENSITY}
+	
+	private boolean trim = true;
+	private int trimStart = 2000;
+	private int trimEnd = Integer.MAX_VALUE;
+	
+    private JFrame frame;
+    private JScrollPane scrollPane;
+    private JButton saveButton;
+    
+    private ArrayList<ChartPanel> chartPanels;
+	private TextArea textArea = null;
+	
+	public ModelStatistics(String title) {
+		
+		//Gets rid of all the fancy graph settings.
+		ChartFactory.setChartTheme(StandardChartTheme.createLegacyTheme());
+		
+		frame = new JFrame();
+		frame.setTitle(title);
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT));
+		
+		scrollPane = new JScrollPane(this);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+		scrollPane.getHorizontalScrollBar().setUnitIncrement(16);
+		
+		frame.add(scrollPane);
+		
+		this.chartPanels = new ArrayList<ChartPanel>();
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		
+		saveButton = new JButton("Save all graphs");
+		saveButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				saveAllCharts();
+			}
+		});
+		buttonPanel.add(saveButton);
+		
+		frame.add(buttonPanel, BorderLayout.SOUTH);
+	}
+
+	public void display() {
+		frame.pack();
+		frame.setVisible(true);
+		saveAllChartsThumbnail();
+	}
+	
+	public void plotDensity(ArrayList<Pair<Double, Double>>[] table, String title,
+			 String xLabel, String experiment) {
+
+		ChartType type = ChartType.HISTOGRAM;
+		//ArrayList<Pair<Double, Double>>[] densityData = Statistics.calculateDensity(Statistics.aggregateArrayLists(table));
+		ChartPanel chartPanel = new ChartPanel(table, type, title + "(Density)", "Occurences", xLabel, experiment);
+		addChartPanel(chartPanel);
+
+		//Do we also trim?
+		if(trim){
+			int length = table[0].size();
+			int trimStart = this.trimStart < length? this.trimStart : 0;
+			int trimEnd = this.trimEnd < length? this.trimEnd : length;
+
+			ArrayList<Pair<Double, Double>>[] trimmedDataArrayList = Statistics.trimArrayLists(table, trimStart, trimEnd);
+			//densityData = Statistics.calculateDensity(Statistics.aggregateArrayLists(trimmedDataArrayList));
+			chartPanel.addAdditionalChart(trimmedDataArrayList, type, title + " (Density Trimmed " + trimStart + "-" + trimEnd+")", "Occurences", xLabel);
+		}
+		
+	}
+	
+	public void plotTimeSeries(ArrayList<Pair<Double, Double>>[] table, String title,
+			String label, String experiment){
+
+		ChartPanel chartPanel = new ChartPanel(table, ChartType.LINE_CHART, title, label, "Generations", experiment);
+		addChartPanel(chartPanel);
+		
+		//Do we also trim?
+		if(trim){
+			int length = table[0].size();
+			int trimStart = this.trimStart < length? this.trimStart : 0;
+			int trimEnd = this.trimEnd < length? this.trimEnd : length;
+			
+			ArrayList<Pair<Double, Double>>[] trimmedDataArrayList = Statistics.trimArrayLists(table, trimStart, trimEnd);
+			chartPanel.addAdditionalChart(trimmedDataArrayList, ChartType.LINE_CHART, title + " (Trimmed " + trimStart + "-" + trimEnd+")", label, "Generations");
+		}
+	}
+	
+	public void addChartPanel(ChartPanel chartPanel){
+		add(chartPanel);
+		chartPanels.add(chartPanel);
+
+		validate();
+		frame.validate();
+	}
+	
+	private void addConsolePanel(String text) {
+		JPanel consolePanel = new JPanel();
+		textArea = new TextArea(text);
+		consolePanel.add(textArea);
+		add(consolePanel);
+		
+		validate();
+		frame.validate();
+	}
+	
+	public void updateConsoleText(String text) {
+		if(textArea==null) addConsolePanel(text); // lazy
+		else textArea.setText(text);
+	}
+	
+	/**
+	 * Save all charts displayed in window. The smaller images are replaced by larger ones.
+	 */
+	public void saveAllCharts() {
+		for (ChartPanel panel : chartPanels) {
+			panel.saveFullSizeChart();
+		}
+	}
+	
+	public void saveAllChartsThumbnail() {
+		for (ChartPanel panel : chartPanels) {
+			panel.saveThumbNailSizeChart();
+		}
+	}
+
+}
