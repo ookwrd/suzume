5e2c489ec9b49327026ad10824b23af39707f96b
luke.mccrohon <luke.mccrohon@localhost>
2011-04-19 15:41:15 +0900

    Changes made during final testing phase, including the addition of new selection model.

---
+++ src/model/SelectionModel.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.util.ArrayList;
+
+import org.apache.commons.collections15.map.CaseInsensitiveMap;
+
+import Agents.Agent;
+
+public abstract class SelectionModel {
+
+	public enum SelectionModels {RouletteWheelSelection, ConstantProbabilitySelection, RandomProbabilitySelection}
+	
+	protected RandomGenerator randomGenerator;
+	
+	public void intialize(RandomGenerator randomGenerator) {
+		this.randomGenerator = randomGenerator;
+	}
+	
+	public abstract ArrayList<Agent> selectAgents(ArrayList<Agent> agents, int number);
+	
+	public static SelectionModel constructSelectionModel(SelectionModels type, RandomGenerator randomGenerator){
+		SelectionModel retVal;
+		
+		switch (type) {
+		case RouletteWheelSelection:
+
+			retVal = new RouletteWheelSelectionModel();
+			break;
+			
+		case RandomProbabilitySelection:
+			
+			retVal = new RandomProbabilitySelectionModel();
+			break;
+			
+		case ConstantProbabilitySelection:
+		default:
+			
+			retVal = new ConstantProbabilitySelectionModel();
+			break;
+		}
+		
+		retVal.intialize(randomGenerator);
+		
+		return retVal;
+	}
+}
