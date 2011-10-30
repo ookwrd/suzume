4f9effbb154192597f3b998d54097e7e8116c355
luke.mccrohon <luke.mccrohon@localhost>
2011-10-30 13:11:50 +0900

    Added gausian meaning distribution to synonym agent.

--- src/nodes/AbstractNode.java
+++ src/nodes/AbstractNode.java
@@ -11,7 +11,7 @@ import simulation.RandomGenerator;
 
 public abstract class AbstractNode extends BasicConfigurable implements Node{
 	
-	public enum NodeType {SimpleConfigurableModel, AdvancedConfigurableModel, YamauchiHashimoto2010Agent, ExtendedYamauchiHashimoto2010Agent, BiasAgent, ProbabilityAgent, SynonymAgent}
+	public enum NodeType {SimpleConfigurableModel, AdvancedConfigurableModel, YamauchiHashimoto2010Agent, ExtendedYamauchiHashimoto2010Agent, ProbabilityAgent, SynonymAgent, BiasAgent}
 	public static final String NODE_TYPE = "Node type";
 	
 	private int id;
